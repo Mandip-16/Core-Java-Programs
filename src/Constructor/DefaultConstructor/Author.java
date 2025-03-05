6243aa77505bd81fdb07290bd00fd9d42dd9fc99
Mandeep1612 <mandeep162003@gmail.com>
2025-03-05 15:58:56 +0530

    Constructor programs

---
+++ src/Constructor/DefaultConstructor/Author.java
@@ -0,0 +1,11 @@
+package Constructor.DefaultConstructor;
+
+class Author {
+    String name;
+    String country;
+
+    public Author() {
+        this.name = "Unknown";
+        this.country = "Unknown";
+    }
+}
